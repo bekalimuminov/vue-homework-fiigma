e48018dee756975563ce2117b7070e3d94745698
bekalimuminov <bekalimuminov@gmail.com>
2026-01-05 00:27:30 +0500

    vue-homework-figma

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import Header from "./components/Header.vue";
+import Hero from "./components/Hero.vue";
+import Menu from "./components/Menu.vue";
+import RectlengeSection from "./components/RectlengeSection.vue";
+import Product from "./components/Product.vue";
+export default {
+  components: {
+    Header,
+    Hero,
+    Menu,
+    RectlengeSection,
+    Product,
+  }
+}
+</script>
+
+<template>
+  <header>
+    <Header/>
+  </header>
+  <main>
+    <Hero/>
+    <Menu/>
+    <RectlengeSection/>
+    <Product/>
+  </main>
+</template>
+
---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="bg-[#F2CEDA] px-[245px] bg-opacity-[30%] ">
+  <div class="">
+    <div class="w-[1125px] mx-auto">
+      <div class="flex items-center justify-between ">
+        <div>
+          <img src="../assets/header-img.svg" alt="img">
+        </div>
+        <nav>
+          <ul class="flex gap-[40px]">
+            <li class="text-[#D23166] text-[15px] font-bold">Home</li>
+            <li class=" text-[15px] font-bold">Product</li>
+            <li class=" text-[15px] font-bold">Pricing</li>
+            <li class=" text-[15px] font-bold">Testimonials</li>
+            <li class=" text-[15px] font-bold">Contact Us</li>
+          </ul>
+        </nav>
+        <div>
+          <button class="bg-[#D23166] py-[14px] px-[50px] rounded-[5px] text-white">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+
+</script>
